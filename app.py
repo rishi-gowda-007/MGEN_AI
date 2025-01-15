8407ec620cec2514d792eca26d24cb12e0d8583d
rishi-gowda-007 <rishugowda89@gmail.com>
2025-01-15 15:50:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise EnvironmentError("SECRET_KEY environment variable is not set!")
+
+from langchain_huggingface import HuggingFaceEndpoint
+os.environ["HF_API_TOKEN"]=SECRET_KEY
+repo_id="mistralai/Mistral-7B-Instruct-v0.3"
+llm=HuggingFaceEndpoint(repo_id=repo_id,max_length=128,temperature=0.7,token=SECRET_KEY)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    output = None
+    if request.method == "POST":
+        input_text = request.form.get("input")
+        if input_text:
+            response = llm.invoke(input_text)
+            response = response.replace('\n', '<br/>')
+            response = response.lstrip('?')
+            output=response
+            print(f"Output: {output}")  
+
+    return render_template("index.html", output=output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
